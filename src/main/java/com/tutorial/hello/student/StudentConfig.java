0695f3f243c81839effc5f255e5997a6e092f6c1
Fitrie <mfitrie78@gmail.com>
2023-07-31 23:05:16 +0800

    add StudentConfig and StudentRepository

---
+++ src/main/java/com/tutorial/hello/student/StudentConfig.java
@@ -0,0 +1,34 @@
+package com.tutorial.hello.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student fitrie = new Student(
+                "Fitrie", 
+                "fit@gmail.com", 
+                LocalDate.of(2000, Month.JANUARY, 20)
+            );
+
+            Student alex = new Student(
+                "alex", 
+                "alex@gmail.com", 
+                LocalDate.of(2000, Month.JANUARY, 20) 
+            );
+
+            repository.saveAll(
+                List.of(fitrie, alex)
+            );
+        };
+    }
+}
--- src/main/java/com/tutorial/hello/student/StudentService.java
+++ src/main/java/com/tutorial/hello/student/StudentService.java
@@ -3,20 +3,32 @@ package com.tutorial.hello.student;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-            new Student(
-                1L, 
-                "Fitrie", 
-                "fit@gmail.com", 
-                LocalDate.of(2000, Month.JANUARY, 20), 
-                21
-            )
-        ); 
+        return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student){
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+
+        studentRepository.save(student);
     }
 }
